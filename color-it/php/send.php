19cd8834a6b361ad35704927a7db6472dae824bc
Kristen Gunn <ohkristen@yahoo.com>
2015-12-15 20:51:37 -0800

    inserted photo and updated the contact us page

--- color-it/php/send.php
+++ color-it/php/send.php
@@ -1,11 +1,11 @@
 <?php
 if(!$_POST) exit;
 
-    $to 	  = 'someemail@somedomain.com'; #Replace your email id...
+    $to 	  = 'ohkristen@creativeKDG.com'; #Replace your email id...
 	$name	  = $_POST['txtname'];
 	$email    = $_POST['txtemail'];
     $comment  = $_POST['txtmessage'];
-        
+
 	if(get_magic_quotes_gpc()) { $comment = stripslashes($comment); }
 
 	 $e_subject = 'You\'ve been contacted by ' . $name . '.';
@@ -14,7 +14,7 @@ if(!$_POST) exit;
 	 $msg .= "$comment\r\n\n";
 	 $msg .= "You can contact $name via email, $email.\r\n\n";
 	 $msg .= "-------------------------------------------------------------------------------------------\r\n";
-								
+
 	 if(@mail($to, $e_subject, $msg, "From: $email\r\nReturn-Path: $email\r\n"))
 	 {
 		 echo "<span class='success-msg'>Thanks for Contacting Us, We will call back to you soon.</span>";
